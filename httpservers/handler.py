ab03d951d704826d9ece4022d8115d873c2375fe
Wilberto Morales <wilbertomorales777@gmail.com>
2020-10-16 21:36:24 -0500

    Add client logging

--- httpservers/handler.py
+++ httpservers/handler.py
@@ -1,3 +1,5 @@
+import os
+
 
 class Handler:
     """
@@ -14,3 +16,8 @@ class Handler:
         content = b"hello, world"
         data = b"HTTP/1.1 200 OK\r\nConnection: close\r\nCONTENT-LENGTH: %d\r\n\r\n%s" % (len(content), content)
         clientsocket.send(data)
+
+    def _log(self, message):
+        print(f'client - {os.getpid()} - {message}')
+
+        return self

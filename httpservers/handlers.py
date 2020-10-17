d85b721c5e9013c348ba80bbb908b19f962c2cf2
Wilberto Morales <wilbertomorales777@gmail.com>
2020-10-17 17:14:06 -0500

    Implement a thread handler

--- httpservers/handlers.py
+++ httpservers/handlers.py
@@ -1,7 +1,7 @@
 from .serial_handler import SerialHandler
 from .forking_handler import ForkingHandler
 from .pre_forking_handler import PreForkingHandler
-
+from .thread_handler import ThreadHandler
 
 HANDLERS = {
     'serial': (SerialHandler, ),
@@ -10,4 +10,5 @@ HANDLERS = {
     'pre-forking-4': (PreForkingHandler, 4),
     'pre-forking-8': (PreForkingHandler, 8),
     'pre-forking-12': (PreForkingHandler, 12),
+    'thread': (ThreadHandler, ),
 }
---
+++ httpservers/thread_handler.py
@@ -0,0 +1,31 @@
+import threading
+from socket import SHUT_RDWR
+from .handler import Handler
+import os
+import sys
+
+
+def _target(handler, clientsocket, address):
+    # handler._log(f'{threading.get_native_id()} - handling request')
+    clientsocket.settimeout(60)
+
+    try:
+        handler._send_hello_world(clientsocket)
+
+        clientsocket.shutdown(SHUT_RDWR)
+        clientsocket.close()
+
+        sys.exit(0)
+
+    except OSError as e:
+        print(str(e))
+        print('x')
+
+
+class ThreadHandler(Handler):
+
+    def handle(self):
+        (clientsocket, address) = self.socket.accept()
+
+        thread = threading.Thread(target=lambda: _target(self, clientsocket, address))
+        thread.start()
